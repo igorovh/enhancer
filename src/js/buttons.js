6afec7ca7027db3f3d3801bcc40b3002cc1f8ac6
α <animekgaming@gmail.com>
2021-11-13 15:04:06 +0100

    Adding support for real vod time

--- src/js/buttons.js
+++ src/js/buttons.js
@@ -2,14 +2,13 @@ let infoChecker;
 
 window.addEventListener('DOMContentLoaded', () => {
     infoChecker = setInterval(() => {
-        const channelInfo = document.querySelector('about-section__panel--content');
-        console.log('test', 'looking for');
+        const channelInfo = document.querySelector('.about-section__panel--content');
         if(!channelInfo) return;
         const buttonsWrapper = document.createElement('div');
         console.log('test', 'found');
         buttonsWrapper.id = 'te-buttons-wrapper';
         buttonsWrapper.innerHTML += `<a href="test"><div class="te-button"></div></a>`;
-        buttonsWrapper.append(buttonsWrapper);
+        channelInfo.append(buttonsWrapper);
         console.log('test', 'creating buttons');
         clearInterval(infoChecker);
     }, 1000);
