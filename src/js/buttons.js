336685710a9438d652632db5f74a6265fcb02b55
α <animekgaming@gmail.com>
2021-11-21 15:27:37 +0100

    Adding support for buttons in panel content

--- src/js/buttons.js
+++ src/js/buttons.js
@@ -1,15 +1,39 @@
-let infoChecker;
+let buttonsChecker;
+
+const links = [
+    {
+        name: 'TwitchTracker',
+        url: 'https://twitchtracker.com/%name%'
+    },
+    {
+        name: 'Emotes',
+        url: 'https://emotes.vopp.top/?name=%name%'
+    },
+    {
+        name: 'xayo.pl',
+        url: 'https://xayo.pl/%name%'
+    }
+];
+
+function getName() {
+    let url = window.location.href.replace(/(^\w+:|^)\/\//, '');
+    return url.split('/')[1];
+}
 
 window.addEventListener('DOMContentLoaded', () => {
-    infoChecker = setInterval(() => {
-        const channelInfo = document.querySelector('.about-section__panel--content');
+    buttonsChecker = setInterval(() => {
+        const channelInfo = document.querySelector('.about-section__panel')?.querySelector('.fqTjdi');
+        if(document.querySelector('#te-buttons-wrapper')) return;
         if(!channelInfo) return;
         const buttonsWrapper = document.createElement('div');
-        console.log('test', 'found');
+        channelInfo.appendChild(buttonsWrapper);
+        console.log(channelInfo.children);
         buttonsWrapper.id = 'te-buttons-wrapper';
-        buttonsWrapper.innerHTML += `<a href="test"><div class="te-button"></div></a>`;
-        channelInfo.append(buttonsWrapper);
-        console.log('test', 'creating buttons');
-        clearInterval(infoChecker);
+        buttonsWrapper.className = 'CoreText-sc-cpl358-0 ibBfde social-media-link-overflow';
+        console.log('[te] Creating buttons');
+        let buttons = [];
+        const name = getName().toLowerCase();
+        links.forEach(link => buttons.push(`<a href="${link.url.replace('%name%', name.toLowerCase())}">${link.name}</a>`));
+        buttonsWrapper.innerHTML += '<br />' + buttons.join(' · ');
     }, 1000);
 });
