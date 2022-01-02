417ae8492cbf12943bb456be1a183a59ef8de7a4
animekkk <animekgaming@gmail.com>
2022-01-02 13:39:18 +0100

    Adding new links to panel

--- src/js/panel.js
+++ src/js/panel.js
@@ -6,6 +6,14 @@ const links = [
         url: 'https://twitchtracker.com/%name%'
     },
     {
+        name: 'SullyGnome',
+        url: 'https://sullygnome.com/channel/%name%'
+    },
+    {
+        name: 'pogu.live',
+        url: 'https://pogu.live/videos/%name%'
+    },
+    {
         name: 'Emotes',
         url: 'https://emotes.vopp.top/?name=%name%'
     },
