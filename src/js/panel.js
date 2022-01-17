159a4458616ae2529623de17a1fb63fd2c713091
animekkk <animekgaming@gmail.com>
2022-01-17 01:37:06 +0100

    Fixing panel getName

--- src/js/panel.js
+++ src/js/panel.js
@@ -32,6 +32,7 @@ function getName() {
     const elements = url.split('/');
     let name = elements[1];
     if(name === 'popout' || elements[0].includes('dashboard')) return elements[2];
+    if(name.includes('?')) name = name.substring(0, name.indexOf('?'));
     return name;
 }
 
