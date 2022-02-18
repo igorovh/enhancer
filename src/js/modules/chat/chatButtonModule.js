003d25bc15b0466f4adc7be90c091cfb1de18f74
animekkk <animekgaming@gmail.com>
2022-02-18 23:23:36 +0100

    little fix

--- src/js/modules/chat/chatButtonModule.js
+++ src/js/modules/chat/chatButtonModule.js
@@ -8,7 +8,6 @@ function callback(element) {
   element.setAttribute('twitch-enhancer', '');
   const settingsButton = document.createElement('div');
   settingsButton.id = 'te-settings-button';
-  settingsButton.title = 'TwitchEnhancer Settings';
   settingsButton.className = 'seventv-menu-button';
   settingsButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="white">
   <path fill-rule="evenodd" clip-rule="evenodd" d="M14.25 11.8195C14.25 13.8522 12.6022 15.5 10.5694 15.5C8.53673 15.5 6.88889 13.8522 6.88889 11.8195C6.88889 9.78674 8.53673 8.1389 10.5694 8.1389C12.6022 8.1389 14.25 9.78674 14.25 11.8195ZM10.5 18C14.6421 18 18 14.6421 18 10.5C18 6.35786 14.6421 3 10.5 3C6.35786 3 3 6.35786 3 10.5C3 14.6421 6.35786 18 10.5 18Z"/>
