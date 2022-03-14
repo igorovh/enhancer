1a07a10c07648af89dc7d7143f01b34f2a4aa553
animekkk <animekgaming@gmail.com>
2022-03-14 23:17:29 +0100

    removing test group

--- src/js/data/groups.js
+++ src/js/data/groups.js
@@ -1,8 +1,4 @@
-export const groups = [{
-    name: 'zjeby',
-    icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cat_November_2010-1a.jpg/1200px-Cat_November_2010-1a.jpg',
-    streamers: ['furazek']
-}];
+export const groups = [];
 
 export async function loadGroups() {
     const data = await fetch('https://teapi.vopp.top/groupBadges');
