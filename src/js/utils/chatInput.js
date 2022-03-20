42807afcb46e1e0ef3f238c4bd4d2830c89949b1
animekkk <animekgaming@gmail.com>
2022-03-20 11:17:04 +0100

    Upadting input

--- src/js/utils/chatInput.js
+++ src/js/utils/chatInput.js
@@ -1,12 +1,20 @@
 export function addText(message, pretty, focus) {
     const input = document.querySelector('textarea[data-a-target="chat-input"]');
-    if(!input) return;
-    let value = input.value || input.textContent;
-    if(pretty && !value.endsWith(' ') && value.length > 0) message = ' ' + message;
-    message = value + message;
-    const nativeInput = Object.getOwnPropertyDescriptor(window.HTMLTextAreaElement.prototype, 'value').set;
-    nativeInput.call(input, message);
-    const event = new Event('input', { bubbles: true });
-    input.dispatchEvent(event);
-    if(focus) input.focus();
+    if(input) {
+        let value = input.value || input.textContent;
+        message = fixMessage(message, value);
+        const nativeInput = Object.getOwnPropertyDescriptor(window.HTMLTextAreaElement.prototype, 'value').set;
+        nativeInput.call(input, message);
+        const event = new Event('input', { bubbles: true });
+        input.dispatchEvent(event);
+        if(focus) input.focus();
+    } else {
+        twitch.getChatInput().props.onChange({ target: { value: fixMessage(message, twitch.getAutocompleteHandler().state.value) }});
+        twitch.getChatInput().focus();
+    }
+}
+
+function fixMessage(message, value) {
+    if(!value.endsWith(' ') && value.length > 0) message = ' ' + message;
+    return value + message;
 }
