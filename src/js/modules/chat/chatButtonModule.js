22feaebbe9ed32b7e98555ec1c3c90f31e5b6f42
Deezor1 <skunks400@gmail.com>
2022-04-15 11:09:35 +0200

    fixes for settings page + styling clip download button

--- src/js/modules/chat/chatButtonModule.js
+++ src/js/modules/chat/chatButtonModule.js
@@ -8,11 +8,11 @@ function callback(element) {
   element.setAttribute('twitch-enhancer', '');
   const settingsButton = document.createElement('div');
   settingsButton.id = 'te-settings-button';
-  settingsButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="white">
+  settingsButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="currentColor">
   <path fill-rule="evenodd" clip-rule="evenodd" d="M14.25 11.8195C14.25 13.8522 12.6022 15.5 10.5694 15.5C8.53673 15.5 6.88889 13.8522 6.88889 11.8195C6.88889 9.78674 8.53673 8.1389 10.5694 8.1389C12.6022 8.1389 14.25 9.78674 14.25 11.8195ZM10.5 18C14.6421 18 18 14.6421 18 10.5C18 6.35786 14.6421 3 10.5 3C6.35786 3 3 6.35786 3 10.5C3 14.6421 6.35786 18 10.5 18Z"/>
   <path fill-rule="evenodd" clip-rule="evenodd" d="M12.386 11.4317C12.2943 11.4581 12.1974 11.4722 12.0972 11.4722C11.5219 11.4722 11.0556 11.0059 11.0556 10.4306C11.0556 10.3303 11.0697 10.2334 11.0961 10.1417C10.9501 10.1036 10.7969 10.0833 10.6389 10.0833C9.6417 10.0833 8.83333 10.8917 8.83333 11.8889C8.83333 12.8861 9.6417 13.6944 10.6389 13.6944C11.6361 13.6944 12.4444 12.8861 12.4444 11.8889C12.4444 11.7309 12.4242 11.5777 12.386 11.4317Z"/>
   </svg>
-  <span class="te-tooltip te-settings">TwitchEnhancer Settings</span>
+  <span class="te-tooltip te-settings te-tooltip-left">TwitchEnhancer Settings</span>
     `;
   tooltip(settingsButton, 'te-settings');
   settingsButton.addEventListener('click', showSettings);
--- src/js/modules/clip/clipModule.js
+++ src/js/modules/clip/clipModule.js
@@ -1,31 +1,34 @@
 import { Module } from '../module.js';
+import { tooltip } from '../../utils/tooltip.js';
 
 export const clipModule = new Module('clip', callback);
 
 function callback(element) {
-    if(!window.location.href.includes('clips.twitch.tv') && !window.location.href.includes('/clip/')) return;
-    element.setAttribute('twitch-enhancer', '');
-    createButton(element);
+  if (!window.location.href.includes('clips.twitch.tv') && !window.location.href.includes('/clip/'))
+    return;
+  element.setAttribute('twitch-enhancer', '');
+  createButton(element);
 }
 
 function createButton(element) {
-    const controls = element.querySelector('.player-controls__left-control-group');
-    const button = document.createElement('a');
-    const link = 'https://clipr.xyz/';
-    button.href = createLink();
-    button.target = '_blank'
-    button.rel = 'noopener noreferrer'
-    button.innerHTML = 'Download clip!'
-    button.id = 'te-clip-download';
-    button.className = 'te-video-button';
-    controls.appendChild(button);
+  const controls = element.querySelector('.player-controls__left-control-group');
+  const link = document.createElement('a');
+  link.href = createLink();
+  link.target = '_blank';
+  link.rel = 'noopener noreferrer';
+  link.id = 'te-clip-download';
+  link.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20px" height="20px" stroke="currentColor" aria-hidden="true">
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"></path>
+    </svg><span class="te-tooltip te-download-clip te-tooltip-top">Download this clip</span>`;
+  controls.appendChild(link);
+  tooltip(link, 'te-download-clip');
 }
 
 function createLink() {
-    return 'https://clipr.xyz/' + getClipId();
+  return 'https://clipr.xyz/' + getClipId();
 }
 
 function getClipId() {
-    const url = new URL(window.location.href);
-    return url.pathname.split('/').reverse()[0];
+  const url = new URL(window.location.href);
+  return url.pathname.split('/').reverse()[0];
 }
