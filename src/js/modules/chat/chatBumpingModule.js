fff6bd0622e12b7f54b09c8d9c86f19d269b5273
animekkk <animekgaming@gmail.com>
2022-04-19 19:22:13 +0200

    adding bumping

---
+++ src/js/modules/chat/chatBumpingModule.js
@@ -0,0 +1,36 @@
+import { Module } from '../module.js';
+import { logger } from '../../utils/logger.js';
+import { getChatMessage, getChatMessages } from '../../utils/twitch.js';
+
+export const chatBumpingModule = new Module('chatMessages', callback);
+
+function callback(element) {
+    element.setAttribute('twitch-enhancer', '');
+    const callback = (mutationList, observer) => {
+        for(const mutation of mutationList) {
+            if(mutation.type === 'childList' && mutation.addedNodes) {
+                for(const message of mutation.addedNodes) {
+                    checkMessage(getChatMessage(message), message);
+                }
+            }
+        }
+    }
+    const chatObserver = new MutationObserver(callback);
+    chatObserver.observe(element, { attributes: true, childList: true });
+    logger.info('Chat bumping observer started.');
+}
+
+function checkMessage(message, element) {
+    if(!message) return;
+    element.setAttribute('data-message-id', message.props?.message?.id);
+    console.log(message, '[te]');
+    const messageContent = message.props?.message?.message;
+    if(messageContent && messageContent.startsWith('`bump ')) { //`bump 5818577831
+        element.remove();
+        const bumpElement = getMessageById(messageContent.split(' ')[1]);
+    }
+}
+
+function getMessageById(id) {
+    return document.querySelector(`div[data-message-id=${id}]`);
+}
