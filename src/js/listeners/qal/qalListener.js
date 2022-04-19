6014bb63e9baa32f553af4bc5564d5db81d52d17
animekkk <animekgaming@gmail.com>
2022-04-19 23:40:47 +0200

    fixing qal

--- src/js/listeners/qal/qalListener.js
+++ src/js/listeners/qal/qalListener.js
@@ -3,7 +3,7 @@ import { Listener } from '../listener.js';
 export const qalListener = new Listener('qal', finder, true, 1000);
 
 function finder(document) {
-    const channelInfo = document.querySelector('.about-section__panel')?.querySelector('.knjGHc');
+    const channelInfo = document.querySelector('.about-section__panel');
     if(!channelInfo || channelInfo.hasAttribute('twitch-enhancer')) return;
     return channelInfo;
 } 
