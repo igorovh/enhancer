21f1cfc0d6180e83875d254a88dcce394bd6aaed
Deezor1 <skunks400@gmail.com>
2022-04-27 16:50:28 +0200

    adding tooltip to bumps

--- src/js/modules/clip/clipModule.js
+++ src/js/modules/clip/clipModule.js
@@ -19,7 +19,7 @@ function createButton(element) {
   link.id = 'te-clip-download';
   link.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20px" height="20px" stroke="currentColor" aria-hidden="true">
     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"></path>
-    </svg><span class="te-tooltip te-download-clip te-tooltip-top">Download this clip</span>`;
+    </svg><span class="te-tooltip te-download-clip te-tooltip-top" style="bottom: 3em">Download this clip</span>`;
   controls.appendChild(link);
   tooltip(link, 'te-download-clip');
 }
