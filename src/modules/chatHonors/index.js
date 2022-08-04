97ef15b62dcb182a6b5b9f44e0ca9d489279a56f
animekkk <animekgaming@gmail.com>
2022-08-04 23:43:41 +0200

    Adding honors

---
+++ src/modules/chatHonors/index.js
@@ -0,0 +1,16 @@
+import * as Peeker from '$Peeker';
+import { findHonor } from '$Utils/honor'
+
+Peeker.registerListener('messageEvent', callback);
+
+function callback(message, data) {
+    const username = data?.props?.message?.user.displayName?.toLowerCase();
+    const name = message.querySelector('.chat-line__username');
+    if(!name || !username) return;
+
+    const honor = findHonor(username);
+    if(!honor) return;
+
+    const color = name.style.color || name.firstChild.firstChild.style.color || 'white';
+    name.style.textShadow = `${color} 0 0 10px`;
+}
---
+++ src/utils/honor.js
@@ -0,0 +1,10 @@
+const testHonors = [
+    {
+        username: 'czarny_animekkk1337',
+        type: 'other'
+    }
+]
+
+export function findHonor(username) {
+    return testHonors.find(honor => honor.username.toLowerCase() === username.toLowerCase());
+}
