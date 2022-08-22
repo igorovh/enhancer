59bdc14634eeb908c97bc2f639477ccc6e34e451
animekkk <animekgaming@gmail.com>
2022-08-22 20:36:21 +0200

    First version of bumps

---
+++ src/modules/chatBumps/index.js
@@ -0,0 +1,44 @@
+import * as Peeker from '$Peeker';
+import * as Logger from '$Logger';
+import { getChatMessagesById } from '$Utils/twitch';
+import { tooltip } from '$Utils/tooltip';
+import Component from './component';
+
+Peeker.registerListener('messageEvent', callback);
+
+function callback(message, data) {
+    const content = data.props.message?.message;
+    if (content && content.trim() === '+1' && data.props.message.reply) {
+        const replyId = data.props.message.reply.parentMsgId;
+        const bumped = getChatMessagesById(replyId)[0]?.element;
+        if (!bumped) return;
+        if (bumped.hasAttribute('te-bumped')) return;
+        Logger.debug(`Bumping ${replyId} message`);
+        bumped.setAttribute('te-bumped', true);
+        message.classList.add('te-bumped'); //TODO Settings - Hide +1 messages
+        //TODO When replying to the same message twice it shows up, idk why
+        const bumps = addBumps(bumped);
+        refreshBumps(bumped, replyId, bumps);
+    }
+}
+
+function addBumps(element, amount = 1) {
+    let bumps = amount;
+    if (element.hasAttribute('te-bumps')) bumps += parseInt(element.getAttribute('te-bumps'));
+    element.setAttribute('te-bumps', bumps);
+    return bumps;
+}
+
+function refreshBumps(element, id, amount) {
+    if (!amount) amount = parseInt(element.getAttribute('te-bumps'));
+    let bumps = element.querySelector('.te-bumps');
+    if (bumps) bumps.remove();
+    const content =
+        element.querySelector('.message') ||
+        element.querySelector('.seventv-message-context') ||
+        element.querySelector('span[data-test-selector="chat-line-message-body"]');
+    if (!content) return;
+    bumps = Component(id, amount);
+    content.appendChild(bumps);
+    tooltip(bumps, `te-bump-${id}`);
+}
