45e1b9c821fa80e1c999966c24b110566812986e
animekkk <animekgaming@gmail.com>
2022-08-23 23:40:24 +0200

    Trying to fix bumps

--- src/modules/chatBumps/index.js
+++ src/modules/chatBumps/index.js
@@ -5,6 +5,7 @@ import { getUsername } from '$Utils/chat';
 import { tooltip } from '$Utils/tooltip';
 import { addOption } from '$Utils/messageMenu';
 import Component from './component';
+import NodeCache from 'node-cache';
 
 Peeker.registerListener('messageEvent', callback);
 
@@ -28,23 +29,37 @@ addOption({
     },
 });
 
+const cache = new NodeCache({ stdTTL: 60, maxKeys: 150, checkperiod: 60 });
+
 function callback(message, data) {
-    const content = data.props.message?.message;
+    const messageId = data.props.message?.id;
+    const content = data.props.message?.messageBody;
+    console.log('[te]', messageId, data.props.message?.id, data.props.message.id, data.props.message);
+    setTimeout(
+        () => console.log('[te]', messageId, data.props.message?.id, data.props.message.id, data.props.message),
+        1000
+    );
     if (content && content.trim() === '+1' && data.props.message.reply) {
         const replyId = data.props.message.reply.parentMsgId;
-        const bumped = getChatMessagesById(replyId)[0]?.element;
+        const bumped = getChatMessagesById(replyId)[0];
         if (!bumped) return;
-        if (bumped.hasAttribute('te-bumped')) return;
-        Logger.debug(`Bumping ${replyId} message`);
-        bumped.setAttribute('te-bumped', true);
-        message.classList.add('te-bumped'); //TODO Settings - Hide +1 messages
+        Logger.debug(`New message with id: ${messageId}`);
+        if (cache.get(messageId)) {
+            Logger.debug(`Message again: ${messageId}`);
+            // message.classList.add('te-bumped');
+            return;
+        }
+        cache.set(messageId, 0, 60);
+        Logger.debug(`Adding to cache: ${messageId}`);
+        // message.classList.add('te-bumped'); //TODO Settings - Hide +1 messages
         //TODO When replying to the same message twice it shows up, idk why
         const bumps = addBumps(bumped);
-        refreshBumps(bumped, replyId, bumps);
+        refreshBumps(bumped.element, replyId, bumps);
     }
 }
 
-function addBumps(element, amount = 1) {
+function addBumps(bumped, amount = 1) {
+    const element = bumped.element;
     let bumps = amount;
     if (element.hasAttribute('te-bumps')) bumps += parseInt(element.getAttribute('te-bumps'));
     element.setAttribute('te-bumps', bumps);
