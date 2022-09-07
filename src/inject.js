8c398000e736d48dbf34610f5713ecd2aa7a5d09
animekkk <animekgaming@gmail.com>
2022-09-07 19:32:02 +0200

    Chaning inject script

--- src/inject.js
+++ src/inject.js
@@ -1,15 +1,6 @@
-const settings = document.createElement('div');
-settings.id = 'te-settings';
-document.body.appendChild(settings);
-
 const head = document.head || document.getElementsByTagName('head')[0] || document.documentElement;
 
 const script = document.createElement('script');
 script.src = chrome.runtime.getURL('index.js');
 
-const fa = document.createElement('script');
-fa.src = 'https://kit.fontawesome.com/955f06605f.js';
-fa.crossOrigin = 'anonymous';
-
-head.appendChild(fa);
 head.appendChild(script);
