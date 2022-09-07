7fb8fa02e6de24615604f6e9c8eae22e09925b4a
animekkk <animekgaming@gmail.com>
2022-09-07 21:42:22 +0200

    Removing unused usercard code

--- src/modules/usercard/component.js
+++ src/modules/usercard/component.js
@@ -109,34 +109,6 @@ const services = {
             totalTime,
         };
     },
-    // vislaud: async (username) => {
-    //     let data = await fetch(`https://vislaud.com/api/chatters?logins=${username}`);
-    //     if (data.status != 200) return;
-    //     data = await data.json();
-    //     if (data.length < 1) return;
-    //     data = data[0];
-    //     let totalTime = 0;
-    //     data.watchtimes.sort((a, b) => {
-    //         return b.watchtime - a.watchtime;
-    //     });
-    //     data.watchtimes.forEach((streamer) => (totalTime += streamer.watchtime * 60));
-    //     const watchtimes = [];
-    //     for (let i = 0; i < 5; i++) {
-    //         const watchtime = data.watchtimes[i];
-    //         if (watchtime)
-    //             watchtimes.push({
-    //                 position: i + 1,
-    //                 streamer: watchtime.streamer.displayName,
-    //                 time: watchtime.watchtime * 60,
-    //             });
-    //     }
-    //     return {
-    //         service: 'vislaud.com/{name}',
-    //         username,
-    //         watchtimes,
-    //         totalTime,
-    //     };
-    // },
     auto: async (username) => {
         const watchtimes = [];
 
@@ -156,15 +128,6 @@ const services = {
         });
 
         return watchtimes[0];
-        // const xayo = await services.xayo(username);
-        // const vislaud = await services.vislaud(username);
-
-        // if (xayo && vislaud) {
-        //     if (xayo.totalTime > vislaud.totalTime) return xayo;
-        //     return vislaud;
-        // }
-        // if (!xayo) return vislaud;
-        // return xayo;
     },
 };
 
