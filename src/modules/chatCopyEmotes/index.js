ad6b048e644d543dd91172f388c0020fe71b1870
animekkk <animekgaming@gmail.com>
2022-09-10 20:12:04 +0200

    Fixing copying emotes

--- src/modules/chatCopyEmotes/index.js
+++ src/modules/chatCopyEmotes/index.js
@@ -4,13 +4,15 @@ import { addText } from '$Utils/chat';
 Peeker.registerListener('messageEvent', callback);
 
 function callback(message) {
-    const emotes = message.querySelectorAll('.chat-line__message--emote');
-    if (emotes.length < 1) return;
-    emotes.forEach((emote) => {
-        emote.addEventListener('contextmenu', (event) => {
-            event.preventDefault();
-            const name = emote.alt.replace(/ /g, '');
-            addText(name, true);
+    setTimeout(() => {
+        const emotes = message.querySelectorAll('.chat-line__message--emote');
+        if (emotes.length < 1) return;
+        emotes.forEach((emote) => {
+            emote.addEventListener('contextmenu', (event) => {
+                event.preventDefault();
+                const name = emote.alt.replace(/ /g, '');
+                addText(name, true);
+            });
         });
-    });
+    }, 100); // Thanks 7TV :)
 }
