99e26395fb0626c3ce69c73c5a379c9fa52d7eeb
animekkk <animekgaming@gmail.com>
2022-10-12 13:54:58 +0200

    Finishing chat island

--- src/modules/chatIsland/index.js
+++ src/modules/chatIsland/index.js
@@ -7,7 +7,36 @@ Peeker.add(() => {
     return chat;
 }, callback);
 
+let isHovering = false;
+
 function callback(chat) {
     const component = Component();
+    component.addEventListener('mouseenter', () => (isHovering = true));
+    component.addEventListener('mouseleave', () => (isHovering = false));
     chat.appendChild(component);
 }
+
+export const queue = [];
+
+let active = false;
+let shownTime = 0;
+
+setInterval(() => {
+    const island = document.querySelector('#te-chat-island');
+    if (!island) return;
+    if (isHovering) return;
+    if (active) {
+        if (shownTime > 5) {
+            island.classList.add('te-chat-island-hidden');
+            active = false;
+            shownTime = 0;
+        }
+        shownTime += 0.5;
+        return;
+    }
+    if (queue.length < 1) return;
+    active = true;
+    const text = queue.shift();
+    island.textContent = text;
+    island.classList.remove('te-chat-island-hidden');
+}, 500);
