1bd815ccd43d4cb42f8e15ff3e81dbaa1553e757
animekkk <animekgaming@gmail.com>
2022-11-09 23:15:10 +0100

    Adding working chat bump list

--- src/modules/chatMessageMenu/component.js
+++ src/modules/chatMessageMenu/component.js
@@ -1,7 +1,7 @@
 export default (options, message, data) => {
     const div = document.createElement('div');
     div.id = 'te-message-menu';
-    if (options?.length < 1) div.innerHTML = '<span>There is no options for this message :</span>';
+    if (options?.length < 1) div.innerHTML = '<span>There is no options for this message :<</span>';
     for (const option of options) {
         const available = option.available(message, data);
         const span = document.createElement('span');
