a6fbab7508181eb9a0c8269c24c20c2ecb45321a
animekkk <animekgaming@gmail.com>
2022-11-10 12:00:13 +0100

    Fixing bump list

--- src/modules/chatBumps/index.js
+++ src/modules/chatBumps/index.js
@@ -50,8 +50,6 @@ addOption({
     text: 'Show bumps',
     icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="te-message-menu-icon"><path d="M313.4 32.9c26 5.2 42.9 30.5 37.7 56.5l-2.3 11.4c-5.3 26.7-15.1 52.1-28.8 75.2H464c26.5 0 48 21.5 48 48c0 25.3-19.5 46-44.3 47.9c7.7 8.5 12.3 19.8 12.3 32.1c0 23.4-16.8 42.9-38.9 47.1c4.4 7.2 6.9 15.8 6.9 24.9c0 21.3-13.9 39.4-33.1 45.6c.7 3.3 1.1 6.8 1.1 10.4c0 26.5-21.5 48-48 48H294.5c-19 0-37.5-5.6-53.3-16.1l-38.5-25.7C176 420.4 160 390.4 160 358.3V320 272 247.1c0-29.2 13.3-56.7 36-75l7.4-5.9c26.5-21.2 44.6-51 51.2-84.2l2.3-11.4c5.2-26 30.5-42.9 56.5-37.7zM32 192H96c17.7 0 32 14.3 32 32V448c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32V224c0-17.7 14.3-32 32-32z"/></svg>`,
     condition: (message, data) => {
-        console.log('[te] msg data', data);
-        console.log('[te] bumpers list on message', data.props.message);
         return data.props.message._enhancer_bumpers?.length > 0;
     },
     available: () => true,
@@ -89,8 +87,6 @@ function bumpMessage(message, data) {
     refreshBumps(message, messageId, addBumps(message), true);
 
     data.props.message._enhancer_bumpers = addBumpers(getUsername(), data);
-    console.log('[te] bumped', data);
-    setTimeout(() => console.log('[te] 1 bumped', data), 2000);
 
     return true;
 }
@@ -109,7 +105,7 @@ function callback(message, data) {
             data?.props?.message?.user.displayName?.toLowerCase() ||
             data?.props?.message?.user.userDisplayName?.toLowerCase() ||
             'unknown';
-        bumped.component.props.message._enhancer_bumpers = addBumpers(username, data);
+        bumped.component.props.message._enhancer_bumpers = addBumpers(username, bumped.component);
 
         if (data.props.message?._enhancer_bumps?.hide) {
             message.classList.add('te-bump-message');
@@ -135,9 +131,8 @@ function callback(message, data) {
 }
 
 function addBumpers(username, data) {
-    const users = data.props.message._enhancer_bumpers ?? [];
+    const users = data.props.message?._enhancer_bumpers ?? [];
     if (!users.includes(username)) users.push(username);
-    console.log('[te]');
     return users;
 }
 
